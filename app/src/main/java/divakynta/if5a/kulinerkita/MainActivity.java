84b1aeec4842d7e18a61c3b278858fb5fa7e2fd8
bilasyivaa <nabila04@mhs.mdp.ac.id>
2022-11-09 16:58:00 +0700

    update 11 nov

--- app/src/main/java/divakynta/if5a/kulinerkita/MainActivity.java
+++ app/src/main/java/divakynta/if5a/kulinerkita/MainActivity.java
@@ -1,23 +1,48 @@
 package divakynta.if5a.kulinerkita;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bnvKulinerKita;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        
+        bnvKulinerKita = findViewById(R.id.bnv_kuliner_kita);
+        bnvKulinerKita.setOnItemReselectedListener(new NavigationBarView.OnItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                    case R.id.menu_beranda
+                        bukaFragment(new FragmentBeranda())
+                return false;
+            }
+        });
     }
 
-    @Override
+     private void bukaFragment(Fragment FRG){
+         FragmentManager FM = getSupportFragmentManager();
+         FragmentTransaction FT = FM.beginTransaction();
+         FT.replace(R.id.fl_container, FRG);
+         FT.commit();
+     }
+
     public  boolean onCreateOptionMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_top_option, menu);
+        getMenuInflater().inflate(R.menu.menu_top, menu);
         return super.onCreateOptionsMenu(menu);
+
     }
 }
